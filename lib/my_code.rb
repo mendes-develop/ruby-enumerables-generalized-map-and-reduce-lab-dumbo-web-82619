c600a0387edd55f32678a7ce0e764d386adf4665
Alex Mendes <mendes.develop@gmail.com>
2019-08-20 15:30:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # Your Code Here
+ def map(array)
+  new = []
+  i = 0
+  while i < array.length do
+    new << yield(array[i])
+  end
+  return new
+end
+
+map([1,2,3]) {|n| n * -1}
+ 
+ def reduce(array, starting_value = 0)
+   yield
+ end
+ 
+
